991b852482dcb65811c4ad8fba1ea4279b495286
Squeaky Beaver <theboxguy659@gmail.com>
2020-11-19 21:02:30 -0600

    Properly implement some things

--- cogs/suggestions.py
+++ cogs/suggestions.py
@@ -1,21 +1,26 @@
 import discord
 from discord.ext import commands
 
+
 class SuggestionCommands(commands.Cog):
-  """Suggestion Commands"""
+    """Suggestion Commands"""
 
-  def __init__(self, bot):
-    self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-  @commands.command(name='suggestgame', aliases=['sg'])
-  async def suggest_game(self, ctx, *args):
-    suggestionChannel = self.bot.get_channel(744413784319328286)
-    await suggestionChannel.send(f'{0} suggested ```css{1}``` at `{2}`', ctx.author.mention, ' '.join(args), )
+    @commands.command(name='suggestgame', aliases=['sg'])
+    async def suggest_game(self, ctx, *args):
+        suggestionChannel = self.bot.get_channel(744413784319328286)
+        suggestion_author = f'{ctx.author.name}#{ctx.author.discriminator}'
 
-  @commands.command(name="test")
-  async def test(self, ctx, *args):
-      await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
+        await suggestionChannel.send(
+            '**{}** suggested: ```css\n{}``` at `{}`'.format(
+                suggestion_author, ' '.join(args), ctx.message.created_at))
+
+    @commands.command(name="test")
+    async def test(self, ctx, *args):
+        await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
 
 
 def setup(bot):
-  bot.add_cog(SuggestionCommands(bot))
+    bot.add_cog(SuggestionCommands(bot))
--- main.py
+++ main.py
@@ -4,26 +4,28 @@ from discord.ext import commands
 import discord
 
 bot = commands.Bot(
-	command_prefix="b!",  # Change to desired prefix
-	case_insensitive=True  # Commands aren't case-sensitive
+    command_prefix="b!",  # Change to desired prefix
+    case_insensitive=True  # Commands aren't case-sensitive
 )
 
 bot.author_id = 557273716782923820  # Change to your discord id!!!
 
-@bot.event 
+
+@bot.event
 async def on_ready():  # When the bot is ready
     print("I'm in")
     print(bot.user)  # Prints the bot's username and identifier
 
+
 extensions = [
-	"cogs.devs", # Commands for devs only
-  "cogs.suggestions" # Commands for everyone else
+    "cogs.devs",  # Commands for devs only
+    "cogs.suggestions"  # Commands for everyone else
 ]
 
 if __name__ == '__main__':  # Ensures this is the file being ran
-	for extension in extensions:
-		bot.load_extension(extension)  # Loads every extension.
+    for extension in extensions:
+        bot.load_extension(extension)  # Loads every extension.
 
 keep_alive()  # Starts a webserver to be pinged.
-token = os.environ.get("DISCORD_BOT_SECRET") 
+token = os.environ.get("DISCORD_BOT_SECRET")
 bot.run(token)  # Starts the bot
