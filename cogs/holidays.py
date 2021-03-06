82d2336fba90bacbdc05c67cca700604e0104c33
Squeaky Beaver <theboxguy659@gmail.com>
2021-03-06 08:51:28 -0600

    Finally fix the dang holidays

--- cogs/holidays.py
+++ cogs/holidays.py
@@ -39,7 +39,7 @@ class Holidays(commands.Cog):
             if "timeanddate.com" in i:
                 res.append(f"[`On This Day in History`]({i})")
                 continue
-            res.append(f"[`{sep[-1].replace('-', ' ').title()}`]({i})")
+            res.append(f"[**{sep[-1].replace('-', ' ').title()}**]({i})")
 
         return res
 
@@ -64,9 +64,9 @@ class Holidays(commands.Cog):
         for i in res:
             res_str += i + "\n"
 
-        e = discord.Embed(title=f"Holidays for {'0' if date.month < 10 else ''}"
-                          f"{date.month}/{'0' if date.day < 10 else ''}{date.day}/{date.year}",
-                          description=res_str, url=f"https://checkiday.com/{date.month}/{date.day}/{date.year}")
+        e = discord.Embed(title=f"Holidays for {'0' if day.month < 10 else ''}"
+                          f"{day.month}/{'0' if day.day < 10 else ''}{day.day}/{day.year}",
+                          description=res_str, url=f"https://checkiday.com/{day.month}/{day.day}/{day.year}")
 
         e.set_footer(text=f"{count} results",
                      icon_url="https://i.pinimg.com/originals/b0/b8/5c/b0b85cd8797638d0c80035f572b0cbd3.jpg")  # I know it's a a jpg, I'm sorry
@@ -81,12 +81,15 @@ class Holidays(commands.Cog):
         cst = pytz.timezone("America/Chicago")
 
         if datetime.now(cst).hour == 0 and not self.already_sent:  # Please work
-            res = self.get_holidays("today")
+            print("In") # Debugging
+            self.already_sent = True
+
+            res = self.get_holidays(dateparser.parse("today"))
             count = len(res)
 
             res_str = ""
             for i in res:
-                res_str += i + "\n"
+                res_str += i + "\n\n"
 
             e = discord.Embed(
                 title=f"Today's Holidays", description=res_str, url="https://checkiday.com/")
@@ -95,8 +98,7 @@ class Holidays(commands.Cog):
                          icon_url="https://i.pinimg.com/originals/b0/b8/5c/b0b85cd8797638d0c80035f572b0cbd3.jpg")
 
             await white_board.send(embed=e)
-            self.already_sent = True
-        elif datetime.now(cst).hour:
+        if datetime.now(cst).hour != 0:
             self.already_sent = False
 
 
