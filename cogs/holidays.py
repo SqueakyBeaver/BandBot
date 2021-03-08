cffe0abf8f9e9261c8c67fbe673a71cdce921785
Squeaky Beaver <theboxguy659@gmail.com>
2021-03-08 07:46:17 -0600

    Beautify code and stuff

--- cogs/holidays.py
+++ cogs/holidays.py
@@ -1,3 +1,4 @@
+from cgitb import reset
 import requests
 from bs4 import BeautifulSoup
 from discord.ext import commands, tasks
@@ -15,12 +16,11 @@ class Holidays(commands.Cog):
 
     def get_holidays(self, date):
         link = f"https://www.checkiday.com/{date.month}/{date.day}/{date.year}"
-        res = []
 
         try:
             site = requests.get(link)
         except Exception as e:
-            return res.append(f"ERROR: {e}")
+            return
 
         soup = BeautifulSoup(site.content, "html.parser")
 
@@ -33,13 +33,21 @@ class Holidays(commands.Cog):
                 links_list.append(i.get('href'))
 
         links_list = list(set(links_list))
+        desc_str = ""
         for i in links_list:
             sep = i.split('/')
 
             if "timeanddate.com" in i:
-                res.append(f"[`On This Day in History`]({i})")
+                desc_str += f"**On This Day in History**\n[Learn More]({i})\n"
                 continue
-            res.append(f"[**{sep[-1].replace('-', ' ').title()}**]({i})")
+            desc_str += f"**{sep[-1].replace('-', ' ').title()}**\n[Learn More]({i})\n"
+
+        res = discord.Embed(title=f"Holidays for {'0' if date.month < 10 else ''}"
+                            f"{date.month}/{'0' if date.day < 10 else ''}{date.day}/{date.year}",
+                            description=desc_str, url=link)
+
+        res.set_footer(text=f"{len(links_list)} results",
+                       icon_url="https://i.pinimg.com/originals/b0/b8/5c/b0b85cd8797638d0c80035f572b0cbd3.jpg")  # I know it's a a jpg, I'm sorry
 
         return res
 
@@ -52,26 +60,11 @@ class Holidays(commands.Cog):
         date = dateparser.parse(day)
 
         res = self.get_holidays(date)
-        if "ERROR" in res[0]:  # Thank you Title case for making this possible
-            return await ctx.send(f"{ctx.author.mention}\n{res[0]}")
 
-        if res[0] is None:
+        if res is None:
             return await ctx.send(f"{ctx.author.mention} No results found :(")
 
-        count = len(res)
-
-        res_str = ""
-        for i in res:
-            res_str += i + "\n"
-
-        e = discord.Embed(title=f"Holidays for {'0' if day.month < 10 else ''}"
-                          f"{day.month}/{'0' if day.day < 10 else ''}{day.day}/{day.year}",
-                          description=res_str, url=f"https://checkiday.com/{day.month}/{day.day}/{day.year}")
-
-        e.set_footer(text=f"{count} results",
-                     icon_url="https://i.pinimg.com/originals/b0/b8/5c/b0b85cd8797638d0c80035f572b0cbd3.jpg")  # I know it's a a jpg, I'm sorry
-
-        await ctx.send(ctx.author.mention, embed=e)
+        await ctx.send(ctx.author.mention, embed=res)
 
     @tasks.loop(minutes=1)
     async def daily_holidays(self):
@@ -81,23 +74,12 @@ class Holidays(commands.Cog):
         cst = pytz.timezone("America/Chicago")
 
         if datetime.now(cst).hour == 0 and not self.already_sent:  # Please work
-            print("In") # Debugging
+            print("In")  # Debugging
             self.already_sent = True
 
             res = self.get_holidays(dateparser.parse("today"))
-            count = len(res)
+            await white_board.send(embed=res)
 
-            res_str = ""
-            for i in res:
-                res_str += i + "\n\n"
-
-            e = discord.Embed(
-                title=f"Today's Holidays", description=res_str, url="https://checkiday.com/")
-
-            e.set_footer(text=f"There are {count} holidays today",
-                         icon_url="https://i.pinimg.com/originals/b0/b8/5c/b0b85cd8797638d0c80035f572b0cbd3.jpg")
-
-            await white_board.send(embed=e)
         if datetime.now(cst).hour != 0:
             self.already_sent = False
 
