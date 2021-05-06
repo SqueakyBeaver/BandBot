2448d2a563499b9a4b88f877eae7b0a68dceeea9
Squeaky Beaver <theboxguy659@gmail.com>
2021-05-06 07:54:40 -0500

    Possibly add the graduating thing

--- src/cogs/general.py
+++ src/cogs/general.py
@@ -11,7 +11,6 @@ class GeneralCommands(commands.Cog, name="general"):
         # bot.annoying_task = self.be_annoying.start()
         self.bot = bot
 
-
         # Channel: message
         self.deleted_messages: dict[discord.TextChannel, discord.Message] = {}
 
@@ -61,7 +60,7 @@ class GeneralCommands(commands.Cog, name="general"):
     async def snipe_deletion(self, ctx: commands.Context):
         if ctx.channel not in self.deleted_messages:
             return await ctx.reply("There were no deletions that I could see.\n"
-                                  "Better luck next time.")
+                                   "Better luck next time.")
 
         embed: discord.Embed = discord.Embed(colour=discord.Colour.green(),
                                              title="I found that filthy deleter")
@@ -81,7 +80,7 @@ class GeneralCommands(commands.Cog, name="general"):
     async def snipe_edit(self, ctx):
         if ctx.channel not in self.edited_messages:
             return await ctx.reply("There were no edits that I can see.\n"
-                                  "Better luck next time.")
+                                   "Better luck next time.")
 
         embed: discord.Embed = discord.Embed(colour=discord.Colour.green(),
                                              title="I see what they tried to hide")
@@ -101,13 +100,36 @@ class GeneralCommands(commands.Cog, name="general"):
 
         await ctx.reply(ctx.author.mention, embed=embed)
 
-    # @tasks.loop(minutes=1)
-    # async def be_annoying(self):
-    #     channel: discord.TextChannel = self.bot.get_channel(767858104066637834)
-    #     user: discord.User = self.bot.get_user(665018712046174228)
-    #     await self.bot.wait_until_ready()
-    #     if datetime.now(tz=pytz.timezone("America/Chicago")).minute % 30 == 0:
-    #         await channel.send("There are no monkes in Africa, {0}\nSource: https://en.wikipedia.org/wiki/Monkeys_in_Africa".format(user.mention))
+    @commands.command(name="newyear",
+                      aliases=["endyear", "graduate"])
+    async def new_year(self, ctx: commands.Context):
+        guild: discord.Guild = ctx.guild
+        roles = {"raisins": guild.get_role(827239098804469780), "senior": guild.get_role(769201494821306368), "junior": guild.get_role(769201510801211452),
+                 "sophomore": guild.get_role(769201512227667999), "freshman": guild.get_role(769201513603137566), "middle school": guild.get_role(769201975346200606)}
+
+        await ctx.typing()
+        for user in ctx.guild.members:
+            if user.bot:
+                continue
+
+            if roles["senior"] in user.roles:
+                await user.remove_roles(roles["senior"])
+                await user.add_roles(roles["raisins"])
+
+            if roles["junior"] in user.roles:
+                await user.remove_roles(roles["junior"])
+                await user.add_roles(roles["senior"])
+
+            if roles["sophomore"] in user.roles:
+                await user.remove_roles(roles["sophomore"])
+                await user.add_roles(roles["junior"])
+
+            if roles["freshman"] in user.roles:
+                await user.remove_roles(roles["freshman"])
+                await user.add_roles(roles["sophomore"])
+
+        await ctx.reply("Done")
+
 
 
 def setup(bot):
