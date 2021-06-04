22f4c3afcdbf8c946feb35756ac32928cb9f52a4
Squeaky Beaver <>
2021-06-04 14:58:07 +0000

    gotta commit real quick

--- src/cogs/daily.py
+++ src/cogs/daily.py
@@ -21,9 +21,10 @@ class Daily(commands.Cog, name="daily"):
     @commands.command(name="test_daily")
     async def _test_daily(self, ctx: commands.Context):
         for (key, value) in self.guilds.items():
-            ping_role: discord.Role = self.bot.get_guild(
-                int(key)).get_role(value["role"])
-        await ctx.reply(content=self.daily_quotes(ping_role), embed=self.daily_holidays())
+            ping_role: discord.Role = self.bot.get_guild(int(key)).get_role(
+                value["role"])
+        await ctx.reply(content=self.daily_quotes(ping_role),
+                        embed=self.daily_holidays())
 
     @tasks.loop(minutes=2)
     async def update(self):
@@ -35,32 +36,37 @@ class Daily(commands.Cog, name="daily"):
     async def daily_message(self):
         print("checking...")
         for (key, value) in self.guilds.items():
-            guild_tz = pytz.timezone(value["tz"]) if value["tz"] else None
-            announcement_channel: discord.TextChannel = self.bot.get_channel(
-                value["channel"])
-            tmp_guild: discord.Guild = self.bot.get_guild(
-                int(key))
-            if tmp_guild is not None:
+            try:
+                guild_tz = pytz.timezone(value["tz"]) if value["tz"] else None
+                announcement_channel: discord.TextChannel = self.bot.get_channel(
+                    value["channel"])
+                tmp_guild: discord.Guild = self.bot.get_guild(int(key))
                 ping_role: discord.Role = tmp_guild.get_role(value["role"])
-            last_sent: datetime = dateparser.parse(value["time"])
+                last_sent: datetime = dateparser.parse(value["time"])
+                print("Last Sent for {0}: {1}".format(
+                    self.bot.get_guild(int(key).name, str(last_sent))))
 
-            # If it has been sent, uncheck the thing
-            if last_sent >= dateparser.parse("Today at 0:00"):
-                value["sent"] = False
-                print("Not sent at {0}".format(datetime.now(pytz.timezone("America/Chicago"))))
+                # If it has been sent, uncheck the thing
+                if last_sent >= dateparser.parse("Today at 0:00"):
+                    value["sent"] = False
+                    print("Not sent at {0}".format(
+                        datetime.now(pytz.timezone("America/Chicago"))))
 
-            # If it is the time to send it and it hasn't been sent today, send it
-            elif datetime.now(guild_tz).hour >= 0 and last_sent < dateparser.parse("Today at 0:00"):
-                if announcement_channel is not None:
-                    tmp_msg: discord.Message = await announcement_channel.send(embed=self.daily_holidays())
+                # If it is the time to send it and it hasn't been sent today, send it
+                elif datetime.now(
+                        guild_tz).hour >= 0 and last_sent < dateparser.parse(
+                            "Today at 0:00"):
+                    tmp_msg: discord.Message = await announcement_channel.send(
+                        embed=self.daily_holidays())
                     await tmp_msg.publish()
-                    await announcement_channel.send(self.daily_quotes(ping_role))
-                value["sent"] = True
-                value["time"] = str(datetime.now(guild_tz))
-                print("Sent at {0}".format(datetime.now(self.tz)))
-            self.info.update("guilds", self.guilds)
-
-
+                    await announcement_channel.send(
+                        self.daily_quotes(ping_role))
+                    value["sent"] = True
+                    value["time"] = str(datetime.now(guild_tz))
+                    print("Sent at {0}".format(datetime.now(self.tz)))
+                self.info.update("guilds", self.guilds)
+            except:
+                continue
 
     def daily_holidays(self):
         holidays: Holidays = Holidays(self.bot)
