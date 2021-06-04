91d25caaecc7e147655693d64e6893ba54286387
Squeaky Beaver <theboxguy659@gmail.com>
2021-06-04 10:44:44 -0500

    Finally made it work hopefully, we'll see

--- src/cogs/daily.py
+++ src/cogs/daily.py
@@ -12,7 +12,7 @@ class Daily(commands.Cog, name="daily"):
     def __init__(self, bot: commands.Bot):
         self.daily_message.start()
         self.update.start()
-        self.bot: commands.Bot = bot
+        self.bot = bot
         self.info: DBClient = DBClient("daily")
         self.guilds: dict = self.info.find("guilds")
         self.tz = pytz.timezone("America/Chicago")
@@ -26,37 +26,43 @@ class Daily(commands.Cog, name="daily"):
         await ctx.reply(content=self.daily_quotes(ping_role),
                         embed=self.daily_holidays())
 
-    @tasks.loop(minutes=2)
+    @tasks.loop(minutes=1)
     async def update(self):
+        await self.bot.wait_until_ready()
         self.info: DBClient = DBClient("daily")
         self.guilds: dict = self.info.find("guilds")
         print(self.guilds)
 
     @tasks.loop(minutes=1)
     async def daily_message(self):
+        await self.bot.wait_until_ready()
         print("checking...")
         for (key, value) in self.guilds.items():
-            try:
-                guild_tz = pytz.timezone(value["tz"]) if value["tz"] else None
-                announcement_channel: discord.TextChannel = self.bot.get_channel(
-                    value["channel"])
-                tmp_guild: discord.Guild = self.bot.get_guild(int(key))
-                ping_role: discord.Role = tmp_guild.get_role(value["role"])
-                last_sent: datetime = dateparser.parse(value["time"])
-                print("Last Sent for {0}: {1}".format(
-                    self.bot.get_guild(int(key).name, str(last_sent))))
+            print(key)
+            if self.bot.get_guild(int(key)):
+                print(self.bot.get_guild(int(key)).name)
 
-                # If it has been sent, uncheck the thing
-                if last_sent >= dateparser.parse("Today at 0:00"):
-                    value["sent"] = False
-                    print("Not sent at {0}".format(
-                        datetime.now(pytz.timezone("America/Chicago"))))
+            guild_tz = pytz.timezone(value["tz"]) if value["tz"] else None
+            last_sent: datetime = dateparser.parse(value["time"])
 
-                # If it is the time to send it and it hasn't been sent today, send it
-                elif datetime.now(
-                        guild_tz).hour >= 0 and last_sent < dateparser.parse(
-                            "Today at 0:00"):
-                    tmp_msg: discord.Message = await announcement_channel.send(
+            tmp_guild = self.bot.get_guild(int(key))
+            ping_role = tmp_guild.get_role(value["role"])
+            print("Last Sent for {0}: {1}".format(
+                tmp_guild.name, str(last_sent)))
+
+            # If it has been sent, uncheck the thing
+            if last_sent >= dateparser.parse("Today at 0:00" + value["time"][-6:]):
+                value["sent"] = False
+                print("Not sent at {0}".format(
+                    datetime.now(pytz.timezone("America/Chicago"))))
+
+            # If it is the time to send it and it hasn't been sent today, send it
+            elif datetime.now(
+                    guild_tz).hour >= 0 and last_sent < dateparser.parse(
+                        "Today at 0:00" + value["time"][-6:]):
+                if announcement_channel := self.bot.get_channel(
+                        value["channel"]):
+                    tmp_msg = await announcement_channel.send(
                         embed=self.daily_holidays())
                     await tmp_msg.publish()
                     await announcement_channel.send(
@@ -64,9 +70,7 @@ class Daily(commands.Cog, name="daily"):
                     value["sent"] = True
                     value["time"] = str(datetime.now(guild_tz))
                     print("Sent at {0}".format(datetime.now(self.tz)))
-                self.info.update("guilds", self.guilds)
-            except:
-                continue
+                    self.info.update("guilds", self.guilds)
 
     def daily_holidays(self):
         holidays: Holidays = Holidays(self.bot)
